8cada55373eaabfc62a7ed2e1eb5118e34837ba5
Masahiro Itabashi <itabasi.lm@gmail.com>
2021-01-07 04:52:54 +0900

    Add function add_node

--- src/bin_search_tree_node.rs
+++ src/bin_search_tree_node.rs
@@ -1,14 +1,13 @@
 //! Author: Masahiro Itabashi <itabasi.lm@gmail.com>
-//! Last modified: Tue, 05 Jan 2021 01:13:34 +0900
+//! Last modified: Thu, 07 Jan 2021 04:51:33 +0900
 extern crate typed_arena;
 
 use std::cell::RefCell;
-//use typed_arena::Arena;
+use std::cmp::Ordering;
+use typed_arena::Arena;
 
-pub struct BinSTreeNode<'a, T>
-    where
-        T: PartialOrd {
-    pub value: RefCell<&'a T>,
+pub struct BinSTreeNode<'a, T: Ord>{
+    pub value: RefCell<T>,
     pub parent: RefCell<Vec<Node<'a, T>>>,
     pub left_child: RefCell<Vec<Node<'a, T>>>,
     pub right_child: RefCell<Vec<Node<'a, T>>>,
@@ -17,10 +16,8 @@ pub struct BinSTreeNode<'a, T>
 type Node<'a, T> = &'a BinSTreeNode<'a, T>;
 
 
-impl<'a, T> BinSTreeNode<'a, T>
-    where
-        T: PartialOrd {
-    pub fn create_node(value: &'a T) -> BinSTreeNode<'a, T> {
+impl<'a, T: Ord> BinSTreeNode<'a, T>{
+    pub fn create_node(value: T) -> BinSTreeNode<'a, T> {
         BinSTreeNode{
             value: RefCell::new(value),
             parent: RefCell::new(vec![]),
@@ -28,4 +25,31 @@ impl<'a, T> BinSTreeNode<'a, T>
             right_child: RefCell::new(vec![]),
         }
     }
+}
+
+pub fn add_node<'a, T: Ord>(tree: &Arena<BinSTreeNode<'a, T>>,
+                                   root_node: Node<'a, T>,
+                                   node: Node<'a, T>){
+
+    match root_node.value.borrow().cmp(&*node.value.borrow()) {
+        Ordering::Greater => {
+            if root_node.left_child.borrow().is_empty() {
+                (*root_node.left_child.borrow_mut()).push(node);
+                (*node.parent.borrow_mut()).push(root_node);
+            }
+            else{
+                add_node(tree, root_node.left_child.borrow()[0], node);
+            }
+        },
+        _ => {
+            if root_node.right_child.borrow().is_empty() {
+                (*root_node.right_child.borrow_mut()).push(node);
+                (*node.parent.borrow_mut()).push(root_node);
+            }
+            else{
+                add_node(tree, root_node.right_child.borrow()[0], node);
+            }
+        },
+    }
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 //! Author: Masahiro Itabashi <itabasi.lm@gmail.com>
-//! Last modified: Mon, 04 Jan 2021 23:42:37 +0900
+//! Last modified: Thu, 07 Jan 2021 04:50:45 +0900
 pub mod parse_args;
 pub mod bin_search_tree_node;
 
 use parse_args::Args;
-use bin_search_tree_node::BinSTreeNode;
+use bin_search_tree_node::{BinSTreeNode, add_node};
+
+use typed_arena::Arena;
 
 fn main(){
     let args: Args = Args::parse_args();
 
-    let node = BinSTreeNode::create_node(&"hoge");
+    let root_node = BinSTreeNode::create_node("aaa".to_string());
+    let node0 = BinSTreeNode::create_node("bbb".to_string());
+    let node1 = BinSTreeNode::create_node("a".to_string());
 
-    println!("{:?}", args);
-    println!("{:?}", node.value);
+    let tree = Arena::new();
+    add_node(&tree, &root_node, &node0);
+    add_node(&tree, &root_node, &node1);
 
+    println!("{:?}", root_node.left_child.borrow()[0].value);
+    println!("{:?}", root_node.right_child.borrow()[0].value);
 }
-
